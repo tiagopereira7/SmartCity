5181aa66b32523fe3b8bcc0a3025e099d6cb13ae
Tiago Pereira <tjosepereira@ipvc.pt>
2020-03-06 16:42:03 +0000

    land and menu

--- app/src/main/java/com/example/smartcity/Menu.java
+++ app/src/main/java/com/example/smartcity/Menu.java
@@ -2,7 +2,9 @@ package com.example.smartcity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -12,4 +14,9 @@ public class Menu extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
     }
+
+    //public void listview(View v){
+       // Intent i = new Intent(Menu.this, listviewFragment.class);
+        //startActivity(i);
+    //}
 }
