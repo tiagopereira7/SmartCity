6a268dc632d6a8f138fadc2bdab8b24f65b391fd
Tiago Pereira <tjosepereira@ipvc.pt>
2020-03-11 18:13:03 +0000

    Botao add

--- app/src/main/java/com/example/smartcity/inserirNotificacao.java
+++ app/src/main/java/com/example/smartcity/inserirNotificacao.java
@@ -3,6 +3,9 @@ package com.example.smartcity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class inserirNotificacao extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class inserirNotificacao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir_notificacao);
+    }
+
+    public void adicionar(View v) {
+        EditText editNome = (EditText) findViewById(R.id.editTitulo);
+        if (editNome.getText().toString().equals("")){
+            Toast.makeText(inserirNotificacao.this, "Preencha o campo titulo ", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
