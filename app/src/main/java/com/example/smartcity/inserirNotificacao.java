03d083577f26ed90ca774eb7dc5cb32e3ddb8df5
Tiago Pereira <tjosepereira@ipvc.pt>
2020-03-23 23:44:48 +0000

    correcao inserir data

--- app/src/main/java/com/example/smartcity/inserirNotificacao.java
+++ app/src/main/java/com/example/smartcity/inserirNotificacao.java
@@ -3,6 +3,7 @@ package com.example.smartcity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +17,7 @@ import android.database.sqlite.SQLiteDatabase;
 
 public class inserirNotificacao extends AppCompatActivity {
 
-    Button add;
+    Button add, update;
     EditText getTitulo;
     EditText getLocal;
     EditText getData;
@@ -31,24 +32,21 @@ public class inserirNotificacao extends AppCompatActivity {
         db = mDbHelper.getReadableDatabase();
         final ListView listView = findViewById(R.id.lista);
         getTitulo =  findViewById(R.id.editTitulo);
-        getLocal =  findViewById(R.id.editLocal);
+        getLocal =  findViewById(R.id.editCidade);
         getData = findViewById(R.id.editData);
 
         add = (Button) findViewById(R.id.add);
+        update = findViewById(R.id.update);
 
 
 
     add.setOnClickListener(new View.OnClickListener(){
 
     public void onClick(View v) {
-        ContentValues cv = new ContentValues();
 
-        cv.put(Contrato.Notas.COLUMN_TITLE, String.valueOf(findViewById(R.id.editTitulo)));
-        cv.put(Contrato.Notas.COLUMN_DATA, String.valueOf(findViewById(R.id.editData)));
-        cv.put(Contrato.Notas.COLUMN_ID_CIDADE, String.valueOf(findViewById(R.id.editLocal)));
         EditText editTitulo = (EditText) findViewById(R.id.editTitulo);
         EditText editData = (EditText) findViewById(R.id.editData);
-        EditText editLocal = (EditText) findViewById(R.id.editLocal);
+        EditText editLocal = (EditText) findViewById(R.id.editCidade);
         if (editTitulo.getText().toString().equals("")){
             Toast.makeText(inserirNotificacao.this, "Preencha o campo Titulo ", Toast.LENGTH_LONG).show();
         }
@@ -59,7 +57,14 @@ public class inserirNotificacao extends AppCompatActivity {
             Toast.makeText(inserirNotificacao.this, "Preencha o campo Local ", Toast.LENGTH_LONG).show();
         }
 
-        mDbHelper.add(editTitulo.getText().toString(), editLocal.getText().toString(), editData.getText().toString());
+        ContentValues cv = new ContentValues();
+
+        cv.put(Contrato.Notas.COLUMN_TITLE, String.valueOf(findViewById(R.id.editTitulo)));
+        cv.put(Contrato.Notas.COLUMN_DATA, String.valueOf(findViewById(R.id.editData)));
+        cv.put(Contrato.Notas.COLUMN_ID_CIDADE, String.valueOf(findViewById(R.id.editCidade)));
+        db.insert(Contrato.Notas.TABLE_NAME, null, cv);
+
+        //mDbHelper.add(editTitulo.getText().toString(), editLocal.getText().toString(), editData.getText().toString());
         Intent output = new Intent();
         setResult(RESULT_OK,output);
         finish();
@@ -67,5 +72,17 @@ public class inserirNotificacao extends AppCompatActivity {
     }
 
     });
+
+    /*update.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) { ContentValues cv = new ContentValues();
+
+        getTitulo.setText(Contrato.Notas.COLUMN_TITLE);
+        getData.setText(Contrato.Notas.COLUMN_DATA);
+        getLocal.setText(Contrato.Cidade.COLUMN_NOME);
+        db.insert(Contrato.Notas.TABLE_NAME, null, cv);
+        }
+    });*/
+
 }
 }
