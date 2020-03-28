832eee46d3fdcea193b12aaf5b06a262597cbea3
Tiago Pereira <tjosepereira@ipvc.pt>
2020-03-28 03:08:14 +0000

    ativity registar

--- app/src/main/java/com/example/smartcity/Registar.java
+++ app/src/main/java/com/example/smartcity/Registar.java
@@ -3,12 +3,24 @@ package com.example.smartcity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Registar extends AppCompatActivity {
+    private EditText editnomeRegisto, editEmailRegisto, editPasswordRegisto, editPasswordConfRegisto;
+    private Button btnAdicionar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registar);
+
+        editnomeRegisto = findViewById(R.id.nomeRegisto);
+        editEmailRegisto = findViewById(R.id.emailRegisto);
+        editPasswordRegisto = findViewById(R.id.passwordRegisto);
+        editPasswordConfRegisto = findViewById(R.id.passwordConfRegisto);
+
+        btnAdicionar = findViewById(R.id.btnAdicionar);
     }
 }
